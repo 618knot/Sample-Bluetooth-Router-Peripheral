7288af29db0db4e295261a03719c0ca45ded26df
618knot <publicac.clt618knot@gmail.com>
2024-12-31 18:53:10 +0900

    serviceとcharacteristicの修正 callback未実装

--- main.py
+++ main.py
@@ -2,46 +2,33 @@ import dbus
 import logging
 from bluezero import peripheral, adapter, async_tools
 import subprocess
-import uuid
 
-# ロギングの設定
 logging.basicConfig(level=logging.DEBUG)
 
-DEVICE_NAME = "Sample Bluetooth Router Peripheral"
+DEVICE_NAME = "BLE Router Peripheral"
 
-# UUIDの定義
-DATA_TRANSFER_SERVICE_UUID = "04871924-adfd-b9a9-96f9-afc7772c0d60"
-UPLOAD_CHARACTERISTIC_UUID = "be1b5b26-c070-7372-6d04-e5214ca64d91"
-DOWNLOAD_CHARACTERISTIC_UUID = "42b1f1c2-4d9a-f841-6097-67eccdeb32b7"
+UPLOAD_SERVICE = "c8edc62d-8604-40c6-a4b4-8878d228ec1c"
+DOWNLOAD_SERVICE = "e1f803e0-7bb5-40a2-a826-2219f54fab56"
 
-write_value = ""
+UPLOAD_SERVICE_ID = 1
+DOWNLOAD_SERVICE_ID = 2
 
-def update_value(characteristic):
-    new_value = uuid.uuid4()
+UPLOAD_DATA_CHARACTERISTIC = "124a03e2-46c2-4ddd-8cf2-b643a1e91071"
+UPLOAD_ID_CHARACTERISTIC = "d7299075-a344-48a7-82bb-2baa19838b2d"
+DOWNLOAD_DATA_CHARACTERISTIC = "b4bf78a1-b41a-4412-b3a9-97740d7003e0"
 
-    characteristic.set_value(new_value)
+UPLOAD_DATA_CHARACTERISTIC_ID = 1
+UPLOAD_ID_CHARACTERISTIC_ID = 2
+DOWNLOAD_DATA_CHARACTERISTIC_ID = 3
 
-    return characteristic.is_notifying
-
-# コールバック関数
-def char_notify(notifying, characteristic):
-    logging.debug(f"Characteristic notify")
-
-    if notifying:
-        async_tools.add_timer_seconds(5, update_value, characteristic)
-
-def char_write(value):
-    logging.debug(f"Characteristic write: {value}")
-
-# システム情報を取得
 def get_system_info():
     info = {}
     info["bluetooth_status"] = subprocess.getoutput("systemctl is-active bluetooth")
     info["dbus_status"] = subprocess.getoutput("systemctl is-active dbus")
     info["bluez_version"] = subprocess.getoutput("bluetoothctl --version")
+
     return info
 
-# 利用可能なBluetoothアダプタを取得
 def get_bluetooth_adapter():
     try:
         adapters = adapter.list_adapters()
@@ -54,57 +41,66 @@ def get_bluetooth_adapter():
         logging.error(f"Error getting Bluetooth adapter: {e}")
         return None
 
-# メイン処理
 def main():
-    # システム情報を表示
-    system_info = get_system_info()
-    logging.info(f"System info: {system_info}")
+  system_info = get_system_info()
+  logging.info(f"System info: {system_info}")
 
-    # Bluetoothアダプタの取得
-    adapter_address = get_bluetooth_adapter()
-    if adapter_address is None:
-        raise Exception("No Bluetooth adapter found")
+  adapter_address = get_bluetooth_adapter()
 
-    logging.info(f"Using Bluetooth adapter: {adapter_address}")
+  if adapter_address is None:
+    raise Exception("No Bluetooth adapter found")
 
-    try:
-        # Peripheral の設定
-        my_peripheral = peripheral.Peripheral(adapter_address, local_name= DEVICE_NAME)
+  logging.info(f"Using Bluetooth adapter: {adapter_address}")
 
-        # サービスの追加
-        my_peripheral.add_service(1, DATA_TRANSFER_SERVICE_UUID, True)
+  try:
+    # Setup Peripheral
+    my_peripheral = peripheral.Peripheral(adapter_address, local_name= DEVICE_NAME)
 
-        # 特性の追加
-        my_peripheral.add_characteristic( 
-            srv_id=1,
-            chr_id=1,
-            uuid=UPLOAD_CHARACTERISTIC_UUID,
-            value=[],
-            notifying=False,
-            flags=["notfiy"],
-            notify_callback=char_notify
-        )
-        
-        my_peripheral.add_characteristic( 
-            srv_id=1,
-            chr_id=2,
-            uuid=DOWNLOAD_CHARACTERISTIC_UUID,
-            value=write_value,
-            notifying=False,
-            flags=["write"],
-            write_callback=char_write
-        )
+    # Add Services
+    my_peripheral.add_service(UPLOAD_SERVICE_ID, UPLOAD_SERVICE, True)
+    my_peripheral.add_service(DOWNLOAD_SERVICE_ID, DOWNLOAD_SERVICE, True)
 
-        # アドバタイジングの開始
-        my_peripheral.publish()
+    # Add Characteristics
+    my_peripheral.add_characteristic(
+      srv_id = UPLOAD_SERVICE_ID,
+      chr_id = UPLOAD_DATA_CHARACTERISTIC_ID,
+      uuid = UPLOAD_DATA_CHARACTERISTIC,
+      value = [],
+      notifying = False,
+      flags = ["read"],
+      notify_callback = None,
+    )
 
-    except dbus.exceptions.DBusException as e:
-        logging.error(f"DBus error: {e}")
-        logging.info("Please check if bluetoothd is running and you have the necessary permissions.")
-    except Exception as e:
-        logging.error(f"Unexpected error: {e}")
-    finally:
-        print("stop")
+    my_peripheral.add_characteristic(
+      srv_id = UPLOAD_SERVICE_ID,
+      chr_id = UPLOAD_ID_CHARACTERISTIC_ID,
+      uuid = UPLOAD_ID_CHARACTERISTIC,
+      value = [],
+      notifying = False,
+      flags = ["notify"],
+      notify_callback = None,
+    )
+
+    my_peripheral.add_characteristic(
+      srv_id = DOWNLOAD_SERVICE_ID,
+      chr_id = DOWNLOAD_DATA_CHARACTERISTIC_ID,
+      uuid = DOWNLOAD_DATA_CHARACTERISTIC,
+      value = [],
+      notifying = False,
+      flags = ["write"],
+      notify_callback = None,
+    )
+
+    # Start Advertising
+    my_peripheral.publish()
+
+  except dbus.exceptions.DBusException as e:
+      logging.error(f"DBus error: {e}")
+      logging.info("Please check if bluetoothd is running and you have the necessary permissions.")
+  except Exception as e:
+      logging.error(f"Unexpected error: {e}")
+  finally:
+      print("stop")
 
 if __name__ == "__main__":
-    main()
+  main()
